23a225c71ac0c95157b09ff9b1f13774d8008373
Lean.Tvener <lean.tvener@ombrello.io>
2025-02-12 12:07:55 +0100

    map update vercel 404 remove

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,47 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import mapData from './mapData.json'
+import { useState } from 'react'
 
 function Map() {
-  return (
+    const [selectedLocation, setSelectedLocation] = useState(null)
+    return (
+      <div>
+            <h1>Map</h1>
+            <div className='map-container'>
+                <div className='map-sidebar'>
+                    <h2>Locations</h2>
+                    <p>Click on a location to view more information</p>
+                    <p> {selectedLocation?.name}</p>
+                </div>
+                <div className='map-map'>
+
     <MapContainer
       center={[mapData.locations[0].latitude, mapData.locations[0].longitude]}
       zoom={13}
       style={{ height: '100%', width: '100%' }}
-    >
+      >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        />
       {mapData.locations.map((location) => (
-        <Marker
+          <Marker
           key={location.id}
           position={[location.latitude, location.longitude]}
-        >
-          <Popup>
+          >
+              <Popup>
+                  
             <h3>{location.name}</h3>
-            <p>{location.description}</p>
+                  <p>{location.description}</p>
+                  <button onClick={() => setSelectedLocation(location)}>Læs mere</button>
           </Popup>
         </Marker>
       ))}
     </MapContainer>
+      </div>
+      </div>
+      </div>
   )
 }
 
