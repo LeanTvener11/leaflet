4174a2de653a269d17f2f3f40d71c394cb98dcc6
Lean.Tvener <lean.tvener@ombrello.io>
2025-02-12 13:05:56 +0100

    adding styling to pin

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import mapData from './mapData.json'
 import { useState } from 'react'
+import { DivIcon } from 'leaflet'
 
 function Map() {
   const [selectedLocation, setSelectedLocation] = useState(null)
@@ -20,25 +21,39 @@ function Map() {
           ]}
           zoom={13}
           style={{ height: '100%', width: '100%' }}
+          scrollWheelZoom={false}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {mapData.locations.map(location => (
-            <Marker
-              key={location.id}
-              position={[location.latitude, location.longitude]}
-            >
-              <Popup>
-                <h3>{location.name}</h3>
-                <p>{location.description}</p>
-                <button onClick={() => setSelectedLocation(location)}>
-                  Læs mere
-                </button>
-              </Popup>
-            </Marker>
-          ))}
+          {mapData.locations.map(location => {
+            const customIcon = new DivIcon({
+              html: `<svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                       <circle cx="16" cy="16" r="16" fill="#c8102e" />
+                     </svg>`,
+              className: `custom-icon ${
+                selectedLocation?.id === location.id ? ' active' : ''
+              }`,
+              iconSize: [32, 32],
+            })
+
+            return (
+              <Marker
+                icon={customIcon}
+                key={location.id}
+                position={[location.latitude, location.longitude]}
+              >
+                <Popup>
+                  <h3>{location.name}</h3>
+                  <p>{location.description}</p>
+                  <button onClick={() => setSelectedLocation(location)}>
+                    Læs mere
+                  </button>
+                </Popup>
+              </Marker>
+            )
+          })}
         </MapContainer>
       </div>
     </div>
